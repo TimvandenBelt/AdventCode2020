de11c591a221dfdb27c05863e54dcd18cd5239be
Tim van den Belt <info@timvandenbelt.com>
2020-12-02 20:34:01 +0100

    Optimize day 1

--- days/day1.py
+++ days/day1.py
@@ -4,30 +4,46 @@ from util import fileUtil
 class Day1:
 	numbers = []
 	numberToForm = 2020
+	calculated = False
+	twoString = False
+	threeString = False
 
 	def setNumbers(self):
 		self.numbers = fileUtil.returnArrayOfLinesForOfFile(fileLocation='./inputs/inputday1', withIntParse=True)
 
-	def part1(self):
-		self.setNumbers()
+	def calculate(self):
+		if not self.calculated:
+			self.setNumbers()
 
 		for number in self.numbers:
-			self.numbers.pop()
-			result = self.checkIfTwoNumbersIsTogether(number)
-			if result:
-				print('Found {}'.format(self.numberToForm))
-				print("{} - {}".format(result[0], result[1]))
+			if self.twoString and self.threeString:
 				return
+
+			self.numbers.pop()
+
+			result = self.checkIfTwoNumbersIsTogether(number)
+			if result and not self.twoString:
+				self.twoString = "{} * {} = {}".format(result[0], result[1], result[0] * result[1])
+
+			result = self.checkIfThreeNumbersIsTogether(number)
+			if result and not self.threeString:
+				self.threeString = "{} * {} * {} = {}".format(result[0], result[1], result[2], result[0] * result[1] * result[2])
+
+	def part1(self):
+		if not self.calculated:
+			self.calculate()
+
+		if self.twoString:
+			print('Found {} with two numbers'.format(self.numberToForm))
+			print(self.twoString)
 
 	def part2(self):
-		self.setNumbers()
-		for number in self.numbers:
-			self.numbers.pop()
-			result = self.checkIfThreeNumbersIsTogether(number)
-			if result:
-				print('Found {}'.format(self.numberToForm))
-				print("{} - {} - {}".format(result[0], result[1], result[2]))
-				return
+		if not self.calculated:
+			self.calculate()
+
+		if self.threeString:
+			print('Found {} with three numbers'.format(self.numberToForm))
+			print(self.threeString)
 
 	def checkIfTwoNumbersIsTogether(self, number):
 		for number2 in self.numbers:
