52992be1d5f00f84b99fed46479e4665622ebe79
Tim van den Belt <info@timvandenbelt.com>
2020-12-03 10:26:53 +0100

    Add day 3

---
+++ days/day3.py
@@ -0,0 +1,66 @@
+from util import fileUtil
+
+
+class TreeRow:
+	spots = []
+
+	def __init__(self, row):
+		self.spots = list(row)
+
+	def getSpotLength(self):
+		return len(self.spots)
+
+	def getSpotPosition(self, x):
+		return self.spots[(x % self.getSpotLength()) - 1]
+
+	def isTreeAtPosition(self, x):
+		char = self.getSpotPosition(x)
+		if char == '#':
+			return True
+		return False
+
+
+class Day3:
+	inputArray = []
+	treeRows = []
+
+	def __init__(self):
+		self.setTreeRows()
+
+	def setTreeRows(self):
+		self.inputArray = fileUtil.returnArrayOfLinesForOfFile(fileLocation='./inputs/inputday3')
+		for line in self.inputArray:
+			self.treeRows.append(TreeRow(line))
+
+	def calculate(self, dy, dx):
+		count = 0
+		rows = len(self.treeRows)
+		y = 0
+		x = 1
+		while rows > y:
+			y += dy
+			x += dx
+			try:
+				if self.treeRows[y].isTreeAtPosition(x):
+					count += 1
+			except IndexError:
+				break
+		return count
+
+	def part1(self):
+		count = self.calculate(1, 3)
+		print('Amount of Trees found default patern')
+		print(count)
+
+	def part2(self):
+		slopes = [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]]
+		count = None
+		for slope in slopes:
+			if count is None:
+				count = self.calculate(slope[0], slope[1])
+				continue
+			count *= self.calculate(slope[0], slope[1])
+
+		print('Amount of Trees found with other patterns')
+		print(count)
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from days.day1 import Day1
 from days.day2 import Day2
+from days.day3 import Day3
 
 # Day 1
 day1 = Day1()
@@ -11,3 +12,7 @@ day2 = Day2()
 day2.part1()
 day2.part2()
 
+# Day 3
+day3 = Day3()
+day3.part1()
+day3.part2()
